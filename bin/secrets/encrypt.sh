a57d2fd90f5fb8b54d0c81963b8e437c50385151
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-04-25 09:53:51 +0200

    update encryption script

--- bin/secrets/encrypt.sh
+++ bin/secrets/encrypt.sh
@@ -3,6 +3,9 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-vault write sops/encrypt/firstkey plaintext=$(base64 <<< $(cat .envrc)) | grep "ciphertext" | cut -d " " -f 6 | >> .envrc.enc
-
-vault write sops/encrypt/firstkey plaintext=$(base64 <<< "Test") | grep "ciphertext" | cut -d " " -f 6 | >> .envrc.enc
+# encrypt enviroment variables
+rm bin/.envrc.enc
+vault write sops/encrypt/firstkey plaintext=$(base64 <<< $(cat bin/.envrc)) | grep "ciphertext" | cut -d " " -f 6 | >> bin/.envrc.enc
+# encrypt ansible vault password
+rm ansible/.vault_pass.enc
+vault write sops/encrypt/firstkey plaintext=$(base64 <<< $(cat ansible/.vault_pass)) | grep "ciphertext" | cut -d " " -f 6 | >> ansible/.vault_pass.enc
