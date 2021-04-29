c55420f58d668d1182a9a52c8653dc558b307f26
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-04-30 00:12:36 +0200

    update decrypt script

--- bin/secrets/decrypt.sh
+++ bin/secrets/decrypt.sh
@@ -5,4 +5,4 @@ rm bin/.env
 vault write sops/decrypt/firstkey ciphertext=$(cat bin/.env.enc) | grep "plaintext" | cut -d " " -f 5 | base64 --decode >> bin/.env
 # decrypt ansible vault password
 rm ansible/.vault_pass
-vault write sops/decrypt/firstkey ciphertext=$(cat ansible/.vault_pass) | grep "plaintext" | cut -d " " -f 5 | base64 --decode >> ansible/.vault_pass
+vault write sops/decrypt/firstkey ciphertext=$(cat ansible/.vault_pass.enc) | grep "plaintext" | cut -d " " -f 5 | base64 --decode >> ansible/.vault_pass
