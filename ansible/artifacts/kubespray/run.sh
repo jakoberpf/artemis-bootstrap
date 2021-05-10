6980547725319a2cc033a77bacc93d926df18fd7
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-05-10 16:31:44 +0200

    update

--- ansible/artifacts/kubespray/run.sh
+++ ansible/artifacts/kubespray/run.sh
@@ -6,5 +6,3 @@ docker run --rm -it --mount type=bind,source="$(pwd)"/inventory/sample,dst=/inve
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 # ansible-playbook -i /inventory/inventory.ini -b --private-key /root/.ssh/id_rsa cluster.yml
-
-https://github.com/kubernetes-sigs/kubespray/tree/master/contrib/terraform/openstack
