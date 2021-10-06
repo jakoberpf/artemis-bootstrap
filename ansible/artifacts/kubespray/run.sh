d74a17def67706ad4e7a17239bf5be3c2e5f9b41
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-10-06 19:20:15 +0200

    updates

--- ansible/artifacts/kubespray/run.sh
+++ ansible/artifacts/kubespray/run.sh
@@ -1,7 +1,7 @@
-docker pull quay.io/kubespray/kubespray:v2.15.1
+docker pull quay.io/kubespray/kubespray:v2.17.0
 docker run --rm -it --mount type=bind,source="$(pwd)"/inventory/sample,dst=/inventory \
   --mount type=bind,source="${HOME}"/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
-  quay.io/kubespray/kubespray:v2.15.1 bash
+  quay.io/kubespray/kubespray:v2.17.0 bash
 # Inside the container you may now run the kubespray playbooks:
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
