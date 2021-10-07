977e2c63906e0fa05b8286ba00773dcec01f6633
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-10-07 12:13:45 +0200

    update run script

--- ansible/artifacts/kubespray/run.sh
+++ ansible/artifacts/kubespray/run.sh
@@ -3,6 +3,6 @@ docker run --rm -it --mount type=bind,source="$(pwd)"/inventory/sample,dst=/inve
   --mount type=bind,source="${HOME}"/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
   quay.io/kubespray/kubespray:v2.17.0 bash
 # Inside the container you may now run the kubespray playbooks:
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+# eval $(ssh-agent -s)
+# ssh-add ~/.ssh/id_rsa
 # ansible-playbook -i /inventory/inventory.ini -b --private-key /root/.ssh/id_rsa cluster.yml
